80b93f5917bda3de47e86455cc5d089a1248c385
Jesse <jesse.szwedko@gmail.com>
2017-02-04 14:35:31 -0800

    Add tests for gracefully handling Google places error
    Also:
    
    * Refactor a bit
    * Change the `puts` to output a log message

--- spec/models/neighborhood_spec.rb
+++ spec/models/neighborhood_spec.rb
@@ -5,14 +5,14 @@ describe Neighborhood do
     let(:mock_client) { double(:mock_client, spots: [mock_spot]) }
     let(:mock_spot) { double(:spot, vicinity: 'Manhattan') }
     let(:empty_client) { double(:empty_client, spots: []) }
-    
+
     context 'when Google finds nearby spots' do
       it 'asks Google for the vicinity of the coordinates' do
         expect(Neighborhood.at(0,0, mock_client)).to eq 'Manhattan'
       end
     end
-    
-    context 'when Google finds no spots' do  
+
+    context 'when Google finds no spots' do
       it 'returns nil' do
         expect(Neighborhood.at(420,420, empty_client)).to eq nil
       end
@@ -23,6 +23,21 @@ describe Neighborhood do
         expect(Neighborhood.at(420,nil, mock_client)).to eq nil
         expect(Neighborhood.at(nil,420, mock_client)).to eq nil
         expect(Neighborhood.at(nil,nil, mock_client)).to eq nil
+      end
+    end
+
+    context 'when an exception occurs' do
+      before do
+        allow(mock_client).to receive(:spots).and_raise("boom")
+      end
+
+      it 'returns nil' do
+        expect(Neighborhood.at(123, 123, mock_client)).to eq nil
+      end
+
+      it 'logs a warning' do
+        expect(Rails.logger).to receive(:warn).with(/error querying for neighborhood.*boom/)
+        Neighborhood.at(123, 123, mock_client)
       end
     end
   end
